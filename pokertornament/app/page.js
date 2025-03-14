f040321b78f7b15d6f6f6904350eeb5299e685e8
Izana180 <valleymouth0402@yahoo.co.jp>
2025-03-14 17:53:39 +0900

    コメント追加

--- pokertornament/app/page.js
+++ pokertornament/app/page.js
@@ -13,16 +13,18 @@ export default function Home() {
     const [inputStartStack, setInputStartStack] = useState(0);
     const [history, setHistory] = useState([]);
     const [currentIntervalIndex, setCurrentIntervalIndex] = useState(0);
-    const [remainingTime, setRemainingTime] = useState(60);
+
     const [isTimerPaused, setIsTimerPaused] = useState(false);
     const [hasPlayedChime, setHasPlayedChime] = useState(false);
     const [chimeAudio, setChimeAudio] = useState(null);
 
     const timeIntervals = [50, 50, 50, 40, 30, 30, 20, 20, 10, 10, 10, 10];
+    const [remainingTime, setRemainingTime] = useState(timeIntervals[0] * 60);
     const blindsLevels = ["5/10", "10/20", "15/30", "20/40", "30/60", "40/80", "50/100", "100/200", "200/400", "300/600", "500/1000", "1000/2000"];
     const anteLevels = [10, 20, 30, 40, 60, 80, 100, 200, 400, 600, 1000, 2000];
     const nextLevelInfo = ["10/20", "15/30", "20/40", "30/60", "40/80", "50/100", "100/200", "200/400", "300/600", "500/1000", "1000/2000", "FINAL LEVEL"];
 
+    // カウントダウン音の有効化(自動再生ポリシーへの対応)
     useEffect(() => {
         const enableAudio = () => {
             setChimeAudio(new Audio("/Time_Signal-Beep01-2(Mid).mp3"));
@@ -39,6 +41,7 @@ export default function Home() {
         };
     }, []);
 
+    // Esc押下時
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.key === 'Escape') {
@@ -52,18 +55,22 @@ export default function Home() {
         };
     }, [isPopupVisible, isTimerPaused]);
 
+    // タイマー
     useEffect(() => {
         if (remainingTime > 0 && !isTimerPaused) {
+            // 1000msごとに1秒減らす
             const timer = setInterval(() => {
                 setRemainingTime((prevTime) => prevTime - 1);
             }, 1000);
 
+            // 2s前になったらカウントダウンを鳴らす
             if (remainingTime <= 2 && !hasPlayedChime && chimeAudio) {
                 chimeAudio.play();
                 setHasPlayedChime(true);
             }
 
             return () => clearInterval(timer);
+        // 0になったらレベルを上げる
         } else if (remainingTime === 0 && currentIntervalIndex < timeIntervals.length - 1) {
             setCurrentIntervalIndex((prevIndex) => prevIndex + 1);
             setRemainingTime(timeIntervals[currentIntervalIndex + 1] * 60);
@@ -71,26 +78,29 @@ export default function Home() {
         }
     }, [remainingTime, currentIntervalIndex, isTimerPaused, hasPlayedChime, chimeAudio]);
 
+    // フロントに出す時用 時間の成形
     const formatTime = (time) => {
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         return `${minutes}:${seconds.toString().padStart(2, "0")}`;
     };
 
+    // 参加人数、残り人数
     const getPlayerRatio = () => {
         if (playerAddCount === 0) return "0 / 0";
         const ratio = (playerAddCount - playerOutCount) / playerAddCount;
         return `${playerAddCount - playerOutCount} / ${playerAddCount}`;
     };
 
+    // アベレージスタックの計算
     const getAvgStack = () => {
         if (playerAddCount === playerOutCount || playerAddCount === 0) return "N/A";
         const avgStack = Math.floor((startStack * playerAddCount) / (playerAddCount - playerOutCount));
         return `${avgStack}`;
     };
 
-    const getPrizePool = () => buyIn * playerAddCount + 1500;
-
+    // プライズプール、配当の計算
+    const getPrizePool = () => buyIn * playerAddCount + 1500; // 1500は大会の仕様上アドオンした数字
     const getPrizeDistribution = () => {
         const prizePool = getPrizePool();
         return {
@@ -100,28 +110,32 @@ export default function Home() {
             fourth: Math.floor(prizePool * 0.1),
         };
     };
+    const prizeDistribution = getPrizeDistribution();
 
+    // 設定メニュー
+    // 決定
     const handleConfirm = () => {
         setBuyIn(inputBuyIn);
         setStartStack(inputStartStack);
         setIsPopupVisible(false);
     };
-
+    // プレイヤー追加
     const handlePlayerAdd = () => {
         setPlayerAddCount(playerAddCount + 1);
-        setHistory([...history, { type: "add" }]);
+        setHistory([...history, { type: "add" }]); // 後のundoのためにアクションの履歴を保持
     };
-
+    // プレイヤーアウト
     const handlePlayerOut = () => {
         if (playerAddCount > playerOutCount) {
             setPlayerOutCount(playerOutCount + 1);
             setHistory([...history, { type: "out" }]);
         }
     };
-
+    // undo
     const handleUndo = () => {
         if (history.length === 0) return;
         const lastAction = history[history.length - 1];
+        // 末尾削除
         setHistory(history.slice(0, -1));
 
         if (lastAction.type === "add") {
@@ -130,8 +144,6 @@ export default function Home() {
             setPlayerOutCount(playerOutCount - 1);
         }
     };
-
-    const prizeDistribution = getPrizeDistribution();
 
     return (
         <div className={Styles.background}>
